199f8c5670475cfdd9b09ec1b4fd2799d52c7e39
mochou <nn.mochou+github@gmail.com>
2026-01-26 16:44:33 +0800

    fix(ci): Build order error

--- scripts/update-dns-rules.sh
+++ scripts/update-dns-rules.sh
@@ -5,13 +5,20 @@ if [ -n "$GITHUB_TOKEN" ]; then
     AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 fi
 
+TARGET_FILE="pkgs/dns-rules.nix"
+
+CURRENT_VERSION=$(grep 'version =' "$TARGET_FILE" | cut -d'"' -f2)
+
 LATEST_TAG=$(curl -s -H "$AUTH_HEADER" https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest | jq -r .tag_name)
 echo "Latest Tag: $LATEST_TAG"
 
+if [ "$LATEST_TAG" == "$CURRENT_VERSION" ]; then
+    echo "Versions match. No update needed. Exiting..."
+    exit 0
+fi
+
 GEOIP_SRI=$(nix hash to-sri --type sha256 $(nix-prefetch-url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_TAG/geoip.dat"))
 GEOSITE_SRI=$(nix hash to-sri --type sha256 $(nix-prefetch-url "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_TAG/geosite.dat"))
-
-TARGET_FILE="pkgs/dns-rules.nix"
 
 sed -i "s/version = \".*\";/version = \"$LATEST_TAG\";/" "$TARGET_FILE"
 
