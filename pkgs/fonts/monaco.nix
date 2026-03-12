eb4241ff3ae14041f1cb5174cfdbb5887554d5eb
mochou <nn.mochou+github@gmail.com>
2026-03-12 21:35:31 +0800

    add fonts

--- pkgs/fonts/monaco.nix
+++ pkgs/fonts/monaco.nix
@@ -14,18 +14,26 @@ let
     sha256 = "sha256-gu1n+GRgiCWBka01B+jrvU2a4T3u9y1/RlspOcWMErE=";
     stripRoot = false;
   };
+
+  MonacoFiraCode = fetchzip {
+    url = "https://github.com/muhac/monaco-fira-code-ligatures/archive/refs/tags/v1.zip";
+    hash = "sha256-z5KXhw0P4ufSvJe49Wf3WG2VaSmIqxWBirLe69cLbuA=";
+    stripRoot = false;
+  };
 in
 stdenv.mkDerivation {
   name = "monaco-nerd-font";
   srcs = [
     MonacoNerdFont
     MonacoNerdFontMono
+    MonacoFiraCode
   ];
   unpackPhase = "true";
   installPhase = ''
     mkdir -p $out/share/fonts/opentype
     cp -r ${MonacoNerdFont}/* $out/share/fonts/opentype
     cp -r ${MonacoNerdFontMono}/* $out/share/fonts/opentype
+    cp -r ${MonacoFiraCode}/monaco-fira-code-ligatures-1/MonacoFiraNerd/* $out/share/fonts/opentype
   '';
 
 }
