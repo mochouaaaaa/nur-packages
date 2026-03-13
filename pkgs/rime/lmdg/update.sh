2ae24e69ad4be1d86848c927bdbe423addceea18
mochou <nn.mochou+github@gmail.com>
2026-03-13 13:43:33 +0800

    fix: update message

--- pkgs/rime/lmdg/update.sh
+++ pkgs/rime/lmdg/update.sh
@@ -9,9 +9,20 @@ TARGET_FILE="$SCRIPT_DIR/default.nix"
 
 ATTR_NAME="${1:-rime.lmdg}"
 
+get_hash() {
+    _hash=nix-instantiate --eval -A $ATTR_NAME.src.hash
+    echo "$_hash"
+}
+
+old_hash=$(get_hash)
+
 update-source-version $ATTR_NAME LTS \
     --file=$TARGET_FILE \
     --ignore-same-hash \
     --ignore-same-version
 
-echo "Update LTS -> LTS"
+new_hash=$(get_hash)
+
+if [ "$old_hash" != "$new_hash" ]; then
+    echo "Update LTS -> LTS"
+fi
